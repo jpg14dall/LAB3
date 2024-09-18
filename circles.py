3f23740afed52a1b1a5729667557f1ecea44207c
JP Rosalita <jprosalita14@gmail.com>
2024-09-18 20:31:00 +0800

    circumference function added

--- circles.py
+++ circles.py
@@ -8,6 +8,11 @@ class Circle:
     
     def diameter(self):
         return (self.radius ** 2)
+    
+    def circumference(self):
+            return (2 * pi * self.radius)
+
+
 
 def radius_validation():
     c = Circle(-42)
@@ -21,11 +26,16 @@ def diameter_test():
     c = Circle(8)
     assert c.diameter() == 16, "Diameter incorrect"
 
+def circumference_test():
+    c = Circle(10)
+    assert round(c.circumference(), 2) == 62.83, "Circumference incorrect"
+
 if __name__ == "__main__":
     functions_to_run = [
         radius_validation,
         radius_test,
-        diameter_test
+        diameter_test,
+        circumference_test
     ]
 
     for fn in functions_to_run:
